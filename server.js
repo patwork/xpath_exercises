98dbde12a1d676ef6d494f55477eb0d6b2460e29
patwork <patwork@gmail.com>
2018-09-18 23:47:57 +0200

    Troche lepiej.

--- server.js
+++ server.js
@@ -6,65 +6,104 @@ var xpath = require('xpath');
 var xmlDom = require('xmldom').DOMParser;
 var PORT = 3000;
 
+// funkcja znajdzWOJTKA
 function findWOJ(req, res) {
-    fs.readFile('data/TERC.xml', 'utf-8', function (error, data) {
-        if (error) console.log(error)
-        var doc = new xmlDom().parseFromString(data)
-        var nodes = xpath.select('//NAZWA|//WOJ|//NAZWA_DOD', doc)
+	fs.readFile('data/TERC.xml', 'utf-8', function(error, data) {
 
-        for (var i = 0; i < nodes.length; i++) {
-            console.log(nodes[i].localName + ": " + nodes[i].firstChild.data)
-        }
-    })
+		// obsluga bledow jakby sie xml nie wczytal zwraca blad HTTP 500
+		if (error) {
+			console.log(error);
+			res.statusCode = 500;
+			res.statusMessage = "Internal Server Error";
+			res.end();
+			return;
+		}
 
-    res.end("Blep");
+		// parsowanie xml
+		var doc = new xmlDom().parseFromString(data);
+		var nodes = xpath.select('//NAZWA|//WOJ|//NAZWA_DOD', doc);
+		var data = [];
+
+		// sklejanie tablicy z danymi
+		for (var i = 0; i < nodes.length; i++) {
+			data.push([i, nodes[i].localName, nodes[i].firstChild.data]);
+		}
+
+		// odsylamy jsona z tablica
+		res.writeHead(200, {
+			'Content-Type': 'application/json'
+		});
+		res.write(JSON.stringify(data));
+		res.end();
+	});
 }
 
-http.createServer(function (req, res) {
-    var url = req.url,
-        ext = path.extname(url),
-        contentType;
+http.createServer(function(req, res) {
+	var url = req.url,
+		ext = path.extname(url),
+		contentType = "text/html"; // domyslny content type
 
-    if (ext == ".js") {
-        contentType = "application/javascript";
-    } else if (ext == ".css") {
-        contentType = "text/css";
-    } else if (ext == ".html") {
-        contentType = "text/html";
-    } else if (ext == ".jpg") {
-        contentType = "image/jpeg";
-    } else if (ext == ".png") {
-        contentType = "image/png";
-    } else if (ext == ".woff") {
-        contentType = "application/font-woff";
-    }
+	// rozpoznanie content type
+	if (ext == ".js") {
+		contentType = "application/javascript";
+	} else if (ext == ".css") {
+		contentType = "text/css";
+	} else if (ext == ".html") {
+		contentType = "text/html";
+	} else if (ext == ".jpg") {
+		contentType = "image/jpeg";
+	} else if (ext == ".png") {
+		contentType = "image/png";
+	} else if (ext == ".woff") {
+		contentType = "application/font-woff";
+	}
 
-    if (url == "/") {
-        switch (req.method) {
-            case "GET":
-                fs.readFile("static/index.html", function (error, data) {
-                    res.writeHead(200, {
-                        'Content-Type': 'text/html'
-                    });
-                    res.write(data);
-                    res.end();
-                })
-                break;
-            case "POST":
-                findWOJ(req, res);
-                break;
-        }
-    } else {
-        fs.readFile("static" + url, function (error, data) {
-            res.writeHead(200, {
-                'Content-Type': contentType
-            });
-            res.write(data);
-            res.end();
-        })
-    }
+	// jezeli "/" to kieruje na index
+	if (url == "/") {
+		url = "/index.html";
+	}
 
-}).listen(PORT)
+	// obsluga getow
+	if (req.method == "GET") {
+		console.log("GET " + url);
 
+		// zapytanie o jsona
+		if (url == "/woj") {
+			findWOJ(req, res);
 
-console.log("Coś tam działa");
+		// wysylanie pliku z static
+		} else {
+			fs.readFile("static" + url, function(error, data) {
+
+				// jezeli nie ma pliku to zwraca HTTP 404
+				if (error) {
+					res.statusCode = 404;
+					res.statusMessage = "Not found";
+					res.end();
+					return;
+				}
+
+				// dane ciurkiem
+				res.writeHead(200, {
+					'Content-Type': contentType
+				});
+				res.write(data);
+				res.end();
+			});
+
+		}
+
+	// obsluga postow
+	} else if (req.method == "POST") {
+		console.log("jaki znowu post jeszcze tego nie obsluguje!");
+
+		// jestem czajnikiem
+		res.statusCode = 418;
+		res.statusMessage = "I’m a teapot";
+		res.end();
+
+	}
+
+}).listen(PORT);
+
+console.log("http://localhost:" + PORT);
--- static/script.js
+++ static/script.js
@@ -1,4 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    getWOJ();
+document.addEventListener("DOMContentLoaded", function() {
+	getWOJ();
+});
 
-})
+function getWOJ() {
+	let xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(this.responseText)
+		}
+	};
+	xhttp.open("GET", "/woj", true);
+	xhttp.setRequestHeader("Content-type", "text/plain");
+	xhttp.send();
+}
